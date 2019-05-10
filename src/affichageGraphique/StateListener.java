2ccf70b0fbcb3d8464fc10024ebd12cf3edcc1b3
LouiseGrall <49550835+LouiseGrall@users.noreply.github.com>
2019-05-10 23:47:24 +0200

    Update StateListener.java

--- src/affichageGraphique/StateListener.java
+++ src/affichageGraphique/StateListener.java
@@ -5,6 +5,10 @@ import java.awt.event.ActionListener;
 
 import javax.swing.JRadioButton;
 
+/**
+* Classe permettant d'écouter un clic sur un bouton
+*@author Paul, Armand et Louise
+*/
 public class StateListener implements ActionListener{
     public void actionPerformed(ActionEvent e) {
       System.out.println("source : " + ((JRadioButton)e.getSource()).getText() + " - état : " + ((JRadioButton)e.getSource()).isSelected());
